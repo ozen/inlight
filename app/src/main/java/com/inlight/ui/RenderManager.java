f3a410191359c6a36cc57835235de15252a702ba
lu1 <lu1@lu1.com>
2015-05-01 20:50:35 +0300

    continous rendering

--- app/src/main/java/com/inlight/ui/RenderManager.java
+++ app/src/main/java/com/inlight/ui/RenderManager.java
@@ -213,7 +213,7 @@ public class RenderManager implements GLSurfaceView.Renderer {
         protected void onPostExecute(Bitmap bitmap) {
 
 
-            mView.requestRender();
+          //  mView.requestRender();
 
             if(bitmap != null) {
                 bitmap.recycle();
